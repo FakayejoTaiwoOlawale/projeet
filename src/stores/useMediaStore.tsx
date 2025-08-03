07f168c614db8b2be172ea792c8b764bbc037fbf
FakayejoTaiwoOlwale <tfakky9@gmail.com>
2025-08-03 15:48:42 +0100

    literally spent the whole day understanding zustand only to copy and paste

--- src/stores/useMediaStore.tsx
+++ src/stores/useMediaStore.tsx
@@ -1,14 +1,41 @@
-import { boolean } from "zod";
-import { create } from "zustand";
+// import { boolean } from "zod";
+// import { create } from "zustand";
 
 
-//this is for deifining like the parameters
-type MediaStore={
-    isLoaded:boolean;
-    setLoaded:(value:boolean)=>void
-}
+// //this is for deifining like the parameters
+// type MediaStore={
+//     isLoaded:boolean;
+//     setLoaded:(value:boolean)=>void
+// }
 
+
+
+
+// export default useMediaStore=create<MediaStore>((set)=>{
+//     isLoaded:boolean,
+//     setLoaded:(value)=>set(isLoaded:value)
+// })
+
+import { create } from 'zustand';
+
+type MediaStore = {
+  isLoaded: boolean;
+  setLoaded: (value: boolean) => void;
+};
+
+export const useMediaStore = create<MediaStore>((set) => ({
+  isLoaded: false,
+  setLoaded: (value) => set({ isLoaded: value }),
+}));
 //this function receives from the type ..its like (thanks security for the )
-export default UseMediaStore=create<MediaStore>((set))
-    isLoaded:false,
-    setLoaded:(value)=>set({isLoaded:value})
+// export default UseMediaStore=create<MediaStore>((set)=>{
+//     isLoaded:false,
+//     setLoaded:()=>set({isLoaded:false})
+// }
+// )
+
+// export default useBearStore=create((set)=>({
+//     bear:1,
+//     addBear:()=>set((state)=>{bear:state.bear+1})
+//     multiplyBear:()=>set((state)=>({bear:state.bear*2}))
+// }))
