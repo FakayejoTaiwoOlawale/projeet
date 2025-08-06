689bceb73265f4d4447ebf00bc38ec28655aebe9
FakayejoTaiwoOlwale <tfakky9@gmail.com>
2025-08-06 07:41:09 +0100

    added the dropdown svg element

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,18 +9,110 @@ const Header = () => {
           <h1 className="text-3xl font-bold text-black">Projeet</h1>
         </div>
         <div className="flex flex-row space-x-6">
-          <p className="hover:text-blue-600 font-semibold text-gray-500">Features</p>
-          <p className="hover:text-blue-600 font-semibold text-gray-500">Solutions</p>
-          <p className="hover:text-blue-600 font-semibold text-gray-500">Home</p>
-          <p className="hover:text-blue-600 font-semibold text-gray-500">Plans</p>
-          <p className="hover:text-blue-600 font-semibold text-gray-500">Pricing</p>
-          <p className="hover:text-blue-600 font-semibold text-gray-500">Resource</p>
+          <p className="hover:text-blue-600 font-semibold text-gray-500">
+            Features{" "}
+            <svg
+              className="w-4 h-4"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+          </p>
+          <p className="hover:text-blue-600 font-semibold text-gray-500">
+            Solutions{" "}
+            <svg
+              className="w-4 h-4"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+          </p>
+          <p className="hover:text-blue-600 font-semibold text-gray-500">
+            Home
+            <svg
+              className="w-4 h-4"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+          </p>
+          <p className="hover:text-blue-600 font-semibold text-gray-500">
+            Plans
+            <svg
+              className="w-4 h-4"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+          </p>
+          <p className="hover:text-blue-600 font-semibold text-gray-500">
+            Pricing{" "}
+            <svg
+              className="w-4 h-4"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+          </p>
+          <p className="hover:text-blue-600 font-semibold text-gray-500">
+            Resource{" "}
+            <svg
+              className="w-4 h-4"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+          </p>
         </div>
       </div>
 
       <div className="flex flex-row items-center">
         <h1 className="text-black text-lg mr-4">Log in</h1>
-        <p className="bg-blue-600 text-lg font-semibold py-4 text-white px-2">Get Projeet for free</p>
+        <p className="bg-blue-600 text-lg font-semibold py-4 text-white px-2">
+          Get Projeet for free
+        </p>
       </div>
     </div>
   );
