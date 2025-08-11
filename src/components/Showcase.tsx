46be1b902359121840b820535355744020a35550
FakayejoTaiwoOlwale <tfakky9@gmail.com>
2025-08-11 17:01:34 +0100

    change the animate directions

--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -12,7 +12,7 @@ const Showcase = () => {
       }}
     >
       <motion.div
-        initial={{ opacity: 0 ,y: -100 }}
+        initial={{ opacity: 0 ,y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
 
